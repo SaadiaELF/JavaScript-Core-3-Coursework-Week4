2a3b55fecf0eaf19847dea8bf4d6cdac5de948ce
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-14 10:51:42 +0000

    Solution old roman

--- C-TDD/01-roman-numerals/convert-to-old-roman.js
+++ C-TDD/01-roman-numerals/convert-to-old-roman.js
@@ -1,5 +1,10 @@
 function convertToOldRoman(n) {
+  let obj = { 1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M" };
 
+  for (const key in obj) {
+    if (n == key) {
+      return obj[key];
+    }
+  }
 }
-
 module.exports = convertToOldRoman;
--- C-TDD/01-roman-numerals/convert-to-old-roman.test.js
+++ C-TDD/01-roman-numerals/convert-to-old-roman.test.js
@@ -1,7 +1,19 @@
 let convertToOldRoman = require("./convert-to-old-roman");
 
-test("returns I if passed 1 as an argument", function () {
-  // Arrange
-  // Act
-  // Assert
+// Arrange
+[
+  { input: 1, expected: "I" },
+  { input: 5, expected: "V" },
+  { input: 10, expected: "X" },
+  { input: 50, expected: "L" },
+  { input: 100, expected: "C" },
+  { input: 500, expected: "D" },
+  { input: 1000, expected: "M" },
+].forEach(({ input, expected }) => {
+  it(`returns ${expected} if passed ${input} as an argument`, () => {
+    // Act
+    let output = convertToOldRoman(input);
+    // Assert
+    expect(output).toBe(expected);
+  });
 });
